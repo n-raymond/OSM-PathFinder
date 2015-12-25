1358f747464c91fe73fc818c0b084f33ec19c2c6
PixelMan <raymond@informatique.univ-paris-diderot.fr>
2015-12-25 19:12:20 +0100

    Add GPL licence on sources codes

--- src/util/listZipper.mli
+++ src/util/listZipper.mli
@@ -1,4 +1,23 @@
 
+(*
+ * Open Street Map PathFinder - Find your way with OpenStreetMap maps
+ * Copyright (C) 2015 - Nicolas TORTRAT / Nicolas Raymond
+
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *)
+
+
 (**	List Zippers
 
     This module give an implementation of list zippers. *)
